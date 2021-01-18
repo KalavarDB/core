db15ed81e971dbeddd9e9a22743d10496407b176
fatalcenturion <tom.b.2k2@gmail.com>
2021-01-18 21:22:12 +0000

    added initial table tests

--- src/tests/structure_tests/core_structures/table.rs
+++ src/tests/structure_tests/core_structures/table.rs
@@ -5,3 +5,9 @@ fn test_new() {
     let table = Table::new("Test Table", vec![]);
     assert_eq!(table.name, "Test Table".to_string())
 }
+
+#[test]
+fn test_new_default_table() {
+    let table = Table::new("Test Table", vec![]);
+    assert!(table.columns.contains_key("__IDENTIFIER__") && table.columns.get("__IDENTIFIER__").unwrap().is_private);
+}
