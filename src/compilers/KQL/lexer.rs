3c518ecb49836b09e5faf040d8e11ab9b990cae2
fatalcenturion <tom.b.2k2@gmail.com>
2021-01-27 12:36:26 +0000

    working on basic parsing of KQL

--- src/compilers/KQL/lexer.rs
+++ src/compilers/KQL/lexer.rs
@@ -19,10 +19,30 @@ impl Lexer {
         }
     }
 
-    fn get_next_char_while(&mut self, raw_token: &mut String, cond: fn(char) -> bool) {
+    pub fn parse(&mut self) -> Result<Vec<Token>> {
+        let mut tokens: Vec<Token> = vec![];
+
+        for token_result in self {
+            match token_result {
+                Ok(token) => {
+                    tokens.push(token);
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+
+        Ok(tokens)
+    }
+
+    fn get_next_char_while(&mut self, raw_token: &mut String, cond: fn(char) -> bool) -> (usize, usize) {
+        let (mut ln, mut ch): (usize, usize) = (0, 0);
         loop {
             match self.raw_data.peek() {
                 Some(c) if cond(*c) => {
+                    ch += 1;
+                    ln += 1;
                     raw_token.push(*c);
                     self.raw_data.next();
                 }
@@ -31,6 +51,8 @@ impl Lexer {
                 }
             }
         }
+
+        (ln, ch)
     }
 
     fn is_identifier(c: char) -> bool {
@@ -42,13 +64,25 @@ impl Iterator for Lexer {
     type Item = self::Result<Token>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let mut ln = 1;
+        let mut ch = 1;
+
         let token: self::Result<Token>;
         let first_char: char;
 
         loop {
             match self.raw_data.next() {
-                Some(c) if c.is_whitespace() => continue,
+                Some(c) if c == '\n' => {
+                    ch = 1;
+                    ln += 1;
+                    continue;
+                }
+                Some(c) if c.is_whitespace() => {
+                    ch += 1;
+                    continue;
+                }
                 Some(c) => {
+                    ch += 1;
                     first_char = c;
                     break;
                 }
@@ -56,29 +90,36 @@ impl Iterator for Lexer {
             }
         }
 
-        
+
         if Self::is_identifier(first_char) && !first_char.is_numeric() {
-                        let mut name = first_char.to_string();
-            self.get_next_char_while(&mut name, Self::is_identifier);
+            let mut name = first_char.to_string();
+            let (l, c) = self.get_next_char_while(&mut name, Self::is_identifier);
+            ln += l;
+            ch += c;
 
             token = Ok(Token::Identifier(name));
         } else if first_char.is_numeric() {
-                        let mut value = first_char.to_string();
-            self.get_next_char_while(&mut value, |c| c.is_numeric());
+            let mut value = first_char.to_string();
+            let (l, c) = self.get_next_char_while(&mut value, |c| c.is_numeric());
+            ln += l;
+            ch += c;
 
             token = match value.parse() {
                 Ok(i) => Ok(Token::Literal(Literal::Integer(i))),
-                Err(_) => Err(format!("Integer literal {} is invalid", value)),
+                Err(_) => Err(format!("Syntax Error ({}:{}): Integer literal {} is invalid", ln, ch, value)),
             }
         } else if first_char == '"' {
-                        let mut value = String::new();
+            let mut value = String::new();
 
-            self.get_next_char_while(&mut value, |c| c != '"');
+            let (l, c) = self.get_next_char_while(&mut value, |c| c != '"');
+            ln += l;
+            ch += c;
+
             self.raw_data.next();
 
             token = Ok(Token::Literal(Literal::Str(value)));
         } else {
-                        let mut raw = first_char.to_string();
+            let mut raw = first_char.to_string();
             loop {
                 if let Some(peek) = self.raw_data.peek() {
                     raw.push(*peek);
@@ -96,11 +137,14 @@ impl Iterator for Lexer {
 
             token = match &raw[..] {
                 s if s == "//" => {
-                                        self.get_next_char_while(&mut String::new(), |c| c != '\n');
+                    let (l, c) = self.get_next_char_while(&mut String::new(), |c| c != '\n');
+                    ln += l;
+                    ch += c;
+
                     self.next()?
                 }
                 s if VALID_SYMBOLS.contains(&s) => Ok(Token::Symbol(raw)),
-                _ => Err(format!("Unknown token: {}", raw)),
+                _ => Err(format!("Syntax Error ({}:{}): Unknown token: {}", ln, ch, raw)),
             }
         }
 
--- src/tests/compiler_tests/KQL.rs
+++ src/tests/compiler_tests/KQL.rs
@@ -1,20 +1,17 @@
 use crate::compilers::KQL::lexer::Lexer;
 
 #[test]
-fn run_lexer_on_text() {
+fn build_token_tree() {
     let mut lexer = Lexer::from_text(r#"GET my_database.A
 FIELDS "name", "email", "pass""#);
 
-    for token_result in lexer {
-        match token_result {
-            Ok(token) => {
-                println!("Token: {:#?}", token);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
+    let compile = lexer.parse();
 
-    assert_eq!(1, 1)
+    if let Ok(tree) = compile {
+        println!("successfully compiled {} tokens", tree.len());
+        assert_eq!(1,1)
+    } else {
+        println!("{}", compile.unwrap_err());
+        assert_eq!(1, 1)
+    }
 }
