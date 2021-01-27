85137c5a2ee84f42de40b9f11c070704ceb0009e
fatalcenturion <tom.b.2k2@gmail.com>
2021-01-27 11:46:11 +0000

    working on basic parsing of KQL

--- src/tests/compiler_tests/KQL.rs
+++ src/tests/compiler_tests/KQL.rs
@@ -3,7 +3,7 @@ use crate::compilers::KQL::lexer::Lexer;
 #[test]
 fn run_lexer_on_text() {
     let mut lexer = Lexer::from_text(r#"GET my_database.A
-FIELDS: "name", "email", "pass""#);
+FIELDS "name", "email", "pass""#);
 
     for token_result in lexer {
         match token_result {
